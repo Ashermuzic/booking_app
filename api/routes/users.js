b1d9813be3ad7870c4c1bc92a1979b698a2a2a9b
Asher <ashersam116@gmail.com>
2023-08-31 08:26:45 -0900

    added json-web-token

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,23 @@
 import express from "express";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updatedUser,
+} from "../controllers/user.js";
 
 const router = express.Router();
+
+//UPDATE
+router.put("/:id", updatedUser);
+
+//DELETE
+router.delete("/:id", deleteUser);
+
+//GET
+router.get("/:id", getUser);
+
+//GETALL
+router.get("/", getUsers);
 
 export default router;
