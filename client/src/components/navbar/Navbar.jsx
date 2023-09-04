f8260b106b5dab6a0d5d7db11b1b592442a12207
Asher <ashersam116@gmail.com>
2023-09-04 04:05:31 -0900

    Login functionally added

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,17 +1,27 @@
+import { useContext } from "react";
 import "./navbar.css";
+import { AuthContext } from "../../context/AuthContext";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="navbar">
       <div className="navContainer">
-        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+        <Link to="/" style={{ color: "white", textDecoration: "none" }}>
           <span className="logo">Asher Booking</span>
         </Link>
-        <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>
+        {user ? (
+          <div style={{ border: "1px solid white", padding: "5px 30px" }}>
+            {user.username}
+          </div>
+        ) : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
+          </div>
+        )}
       </div>
     </div>
   );
