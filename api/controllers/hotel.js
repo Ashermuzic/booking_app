fd17b695b56555dec681dd2026e1da39a4231e65
Asher <ashersam116@gmail.com>
2023-09-03 22:50:08 -0900

    good up to search functionally

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -1,6 +1,6 @@
 import Hotel from "../models/Hotel.js";
 
-export const createHotel = async (req, res) => {
+export const createHotel = async (req, res, next) => {
   const newHotel = new Hotel(req.body);
   try {
     const saveHotel = await newHotel.save();
@@ -44,9 +44,54 @@ export const getHotel = async (req, res) => {
 };
 
 export const getHotels = async (req, res, next) => {
+  const { min, max, limit, ...others } = req.query;
+
   try {
-    const hotels = await Hotel.find(req.params.id);
+    const query = Hotel.find({
+      ...others,
+      cheapestPrice: { $gt: min || 1, $lt: max || 999 },
+    });
+
+    if (limit) {
+      query.limit(parseInt(limit, 10));
+    }
+
+    const hotels = await query.exec();
     res.status(200).json(hotels);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(",");
+  try {
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
+    res.status(200).json(list);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const countByType = async (req, res, next) => {
+  try {
+    const hotelCount = await Hotel.countDocuments({ type: "hotel" });
+    const apartmentCount = await Hotel.countDocuments({ type: "apartment" });
+    const resortCount = await Hotel.countDocuments({ type: "resort" });
+    const villaCount = await Hotel.countDocuments({ type: "villa" });
+    const cabinCount = await Hotel.countDocuments({ type: "cabin" });
+
+    res.status(200).json([
+      { type: "hotel", count: hotelCount },
+      { type: "apartments", count: apartmentCount },
+      { type: "resorts", count: resortCount },
+      { type: "villas", count: villaCount },
+      { type: "cabins", count: cabinCount },
+    ]);
   } catch (err) {
     next(err);
   }
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { createError } from "../utils/error.js";
 import {
+  countByCity,
+  countByType,
   createHotel,
   deleteHotel,
   getHotel,
@@ -21,9 +23,12 @@ router.put("/:id", verifyAdmin, updatedHotel);
 router.delete("/:id", verifyAdmin, deleteHotel);
 
 //GET
-router.get("/:id", getHotel);
+router.get("/find/:id", getHotel);
 
 //GETALL
 router.get("/", getHotels);
+
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
 
 export default router;
